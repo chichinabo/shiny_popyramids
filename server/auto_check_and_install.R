3970413c0912d546b6ebc01cf59080b8a01a0863
Benito M. Zaragozí <benizar@gmail.com>
2015-11-17 13:51:09 +0100

    Create auto_check_and_install.R

---
+++ server/auto_check_and_install.R
@@ -0,0 +1,8 @@
+#A short script to help installing packages on the go
+#Most useful if you are distributing  a set of script files to people who may not be aware that the needed packages are not installed
+#Also useful if you use many packages and want to organise their loading at the beginning of a script
+need<-c("RCurl","shiny","leaflet","RJSONIO","epade","png","grid","RPostgreSQL") #needed packages
+ins<-installed.packages()[,1] #find out which packages are installed
+(Get<-need[which(is.na(match(need,ins)))]) # check if the needed packages are installed
+if(length(Get)>0){install.packages(Get,repos='https://cran.rstudio.com/')} #install the needed packages if they are not-installed
+eval(parse(text=paste("library(",need,")")))#load the needed packages
