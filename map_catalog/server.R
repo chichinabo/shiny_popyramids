7ef0c8f6168fe7244c23d3c38858125348f995ba
benizar <benizar@gmail.com>
2015-11-18 13:46:57 +0100

    Show pyramid on click

--- map_catalog/server.R
+++ map_catalog/server.R
@@ -11,12 +11,26 @@ server <- function(input, output, session) {
     main_map
   })
   
-  #Get all reactive pyramids from leaflet map
+#   #Get all reactive pyramids from leaflet map
   reactive_pyramids<-eventReactive(c(input$whose_provider, input$what_project, input$main_map_bounds, input$when_range), {
-    pids <- get_pids(input$whose_provider,input$what_project, input$main_map_bounds$north, input$main_map_bounds$east, input$main_map_bounds$south, input$main_map_bounds$west, input$when_range[1], input$when_range[2], 1000)
+    pids <- get_pids(input$whose_provider,input$what_project, input$main_map_bounds$north, input$main_map_bounds$east, input$main_map_bounds$south, input$main_map_bounds$west, input$when_range[1], input$when_range[2], 500)
     pyramids <- get_pyramids(pids, input$yscale, input$xscale)
     
     geojson<-RJSONIO::fromJSON(pyramids$pyramids)
+    
+    geojson
+  })
+  
+  #Get the selected reactive pyramid from leaflet map
+  reactive_pyramid<-eventReactive(c(input$whose_provider, input$what_project, input$main_map_marker_click, input$when_range), {
+
+    click<<-input$main_map_marker_click
+    pids<-click$id
+    pid<<-as.data.frame(pids)
+
+    pyramids <<- get_pyramids(pid, input$yscale, input$xscale)
+    
+    geojson<<-RJSONIO::fromJSON(pyramids$pyramids)
     
     geojson
   })
@@ -30,19 +44,36 @@ server <- function(input, output, session) {
     
     df<-geojson_properties(geojson)
     
-
-leafletProxy('main_map')%>%
-  clearMarkerClusters%>%
-  addMarkers(
-    data=df,
-    icon = pyramidIcons[df$what_shape],
-    clusterOptions = markerClusterOptions(), clusterId = 'cluster1',
-    popup=df$how_popup_html_long
-  )
-
-
-
+    
+    leafletProxy('main_map')%>%
+      clearMarkerClusters%>%
+      addMarkers(
+        data=df,
+        layerId = df$pid,
+        icon = pyramidIcons[df$what_shape],
+        clusterOptions = markerClusterOptions(), clusterId = 'cluster1',
+        popup=df$how_popup_html_long
+      )
+    
   })
-
-
+  
+  
+  ##Plot pyramids
+  output$pyramid <- renderPlot({
+    
+    if (is.null(input$main_map_marker_click)){return()}#CHECK: When the app is started, no map bounds are still available
+    geojson <- reactive_pyramid()
+    if (is.null(geojson$features)){return()}#CHECK: When there are no features back, don't do anything
+    
+    
+    if(length(geojson$features)>0){
+      plot_pyramid(geojson$features[[1]])
+    }else{
+      #show empty pyramid
+      plot_pyramid_empty()
+    }
+  })
+  
+  
+  
 }
--- map_catalog/ui.R
+++ map_catalog/ui.R
@@ -4,7 +4,7 @@ ui <- bootstrapPage(
   absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE, 
                 top = 20, left = "auto", right = 20, bottom = "auto",
                 h2("Map-catalog"),
-                p("This app shows only the first 1000 pyramids"),
+                p("This app shows only the first 500 pyramids"),
                 style='padding: 5px;  background-color: rgba(255,255,255,0.75);',
                 selectInput(inputId="whose_provider", label="whose_provider:", choices = global_ui_choices$whose_provider_short),
                 selectInput(inputId="what_project", label="what_project:", choices = global_ui_choices$what_project_short,selected = global_ui_choices$what_project_short[1]),
@@ -14,5 +14,7 @@ ui <- bootstrapPage(
                             selected = "10 years"),
                 selectInput(inputId="xscale",label='X-Scale:',
                             choices = list("Habs." = "false", "Perc. (%)" = "true"), 
-                            selected = 1))
+                            selected = 1),
+                plotOutput("pyramid")
+                )
   )
