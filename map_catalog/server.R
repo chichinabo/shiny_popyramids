367dc317817f84b7f38aae3926cd196dba3e4971
benizar <benizar@gmail.com>
2015-11-19 16:42:49 +0100

    reactive pyramids accepting scale parameters

--- map_catalog/server.R
+++ map_catalog/server.R
@@ -12,17 +12,18 @@ server <- function(input, output, session) {
   })
   
 #   #Get all reactive pyramids from leaflet map
-  reactive_pyramids<-eventReactive(c(input$whose_provider, input$what_project, input$main_map_bounds, input$when_range), {
+  reactive_pyramids<-eventReactive(c(input$whose_provider, input$what_project, input$main_map_bounds, input$when_range, input$yscale, input$xscale), {
     pids <- get_pids(input$whose_provider,input$what_project, input$main_map_bounds$north, input$main_map_bounds$east, input$main_map_bounds$south, input$main_map_bounds$west, input$when_range[1], input$when_range[2], 500)
     pyramids <- get_pyramids(pids, input$yscale, input$xscale)
     
     geojson<-RJSONIO::fromJSON(pyramids$pyramids)
     
     geojson
+    
   })
   
   #Get the selected reactive pyramid from leaflet map
-  reactive_pyramid<-eventReactive(c(input$whose_provider, input$what_project, input$main_map_marker_click, input$when_range), {
+  reactive_pyramid<-eventReactive(c(input$whose_provider, input$what_project, input$main_map_marker_click, input$when_range, input$yscale, input$xscale), {
 
     click<<-input$main_map_marker_click
     pids<-click$id
