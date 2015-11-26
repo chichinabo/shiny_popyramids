64d651f82d8e8261875002071b642a10a34fb69f
benizar <benizar@gmail.com>
2015-11-26 18:29:53 +0100

    Dashboard layout

--- give_me_five/server.R
+++ give_me_five/server.R
@@ -110,4 +110,5 @@ server <- function(input, output, session) {
   })
   
   
+  
 }
--- give_me_five/ui.R
+++ give_me_five/ui.R
@@ -1,40 +1,51 @@
-ui <- bootstrapPage(
-  tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
-  leafletOutput("main_map", width = "100%", height = "100%"),
-  absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
-                top = 20, left = "auto", right = 20, bottom = "auto",
-                h2("Give me five!"),
-                style='padding: 5px;  background-color: rgba(255,255,255,0.75);',
-                selectInput(inputId="whose_provider", label="whose_provider:", choices = global_ui_choices$whose_provider_short),
-                selectInput(inputId="what_project", label="what_project:", choices = global_ui_choices$what_project_short,selected = global_ui_choices$what_project_short[1]),
-                sliderInput("when_range", "when_range:", min = 1981, max = 2016, value = c(2000,2014)),
-                selectInput(inputId="yscale",label='Y-Scale:',
-                            choices = list("Raw data" = "Raw data", "5 years" = "5 years", "10 years" = "10 years", "Big groups" = "Big groups"), 
-                            selected = "10 years"),
-                
-                selectInput(inputId="xscale",label='X-Scale:',
-                            choices = list("Habs." = "false", "Perc. (%)" = "true"), 
-                            selected = 1)),
-  absolutePanel(id = "pyramid1", class = "panel panel-default", fixed = TRUE,
-                draggable = TRUE, top = "auto", left = "auto", right = 400, bottom = 200,
-                width = 450, height = 400,
-                plotOutput("pyramid1", '100%','100%')),
-  absolutePanel(id = "pyramid2", class = "panel panel-default", fixed = TRUE,
-                draggable = TRUE, top = "auto", left = "auto", right = 380, bottom = 160,
-                width = 450, height = 400,
-                plotOutput("pyramid2", '100%','100%')),
-  absolutePanel(id = "pyramid3", class = "panel panel-default", fixed = TRUE,
-                draggable = TRUE, top = "auto", left = "auto", right = 360, bottom = 120,
-                width = 450, height = 400,
-                plotOutput("pyramid3", '100%','100%')),
-  absolutePanel(id = "pyramid4", class = "panel panel-default", fixed = TRUE,
-                draggable = TRUE, top = "auto", left = "auto", right = 340, bottom = 80,
-                width = 450, height = 400,
-                plotOutput("pyramid4", '100%','100%')),
-  absolutePanel(id = "pyramid5", class = "panel panel-default", fixed = TRUE,
-                draggable = TRUE, top = "auto", left = "auto", right = 320, bottom = 40,
-                width = 450, height = 400,
-                plotOutput("pyramid5", '100%','100%'))
-                
-                
+############################ ui ############################
+ui<-dashboardPage(
+  dashboardHeader(
+    title = "Give me five!!"
+  ),
+  dashboardSidebar(
+    
+    sidebarMenu(
+      menuItem("Dashboard", icon=icon("dashboard"), tabName = "dashboard"),
+      menuItem("Filters", icon=icon("filter"), tabName = "filters",
+               selectInput(inputId="whose_provider", label="whose_provider:", choices = global_ui_choices$whose_provider_short),
+               selectInput(inputId="what_project", label="what_project:", choices = global_ui_choices$what_project_short, selected = global_ui_choices$what_project_short[1]),
+               sliderInput("when_range", "when_range:", min = 1981, max = 2016, value = c(2000,2014)),
+               selectInput(inputId="yscale",label='Y-Scale:',
+                           choices = list("Raw data" = "Raw data", "5 years" = "5 years", "10 years" = "10 years", "Big groups" = "Big groups"), 
+                           selected = "10 years"),
+               
+               selectInput(inputId="xscale",label='X-Scale:',
+                           choices = list("Habs." = "false", "Perc. (%)" = "true"), 
+                           selected = 1)
+      ),
+      menuItem("Info", icon=icon("info"), tabName = "info",
+               menuSubItem("Blog", icon=icon("book"), href = "http://www.gisandchips.org/"),
+               menuSubItem("Twitter", icon=icon("twitter-square"), href = "https://twitter.com/gisandchips"),
+               menuSubItem("Facebook", icon=icon("facebook-square"), href = "https://twitter.com/gisandchips")
+               
+      )
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem("dashboard",
+              fluidRow(
+                column(width = 4,
+                       box(width = 12,leafletOutput("main_map")),
+                       box(width = 12, plotOutput("pyramid1"))
+                ),
+                column(width = 4,
+                       box(width = 12, plotOutput("pyramid2")),
+                       box(width = 12, plotOutput("pyramid3"))
+                ),
+                column(width = 4,
+                       box(width = 12, plotOutput("pyramid4")),
+                       box(width = 12, plotOutput("pyramid5"))
+                )
+              )
+      ),
+      tabItem("blog")
+    )
   )
+)
